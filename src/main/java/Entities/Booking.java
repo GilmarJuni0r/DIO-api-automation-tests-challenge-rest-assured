e916f2558b5962bf4bad8179ec1d61401c2b6819
GilmarJuni0r <thejumare@gmail.com>
2023-01-24 14:41:07 -0300

    Restful booker - api automation tests

---
+++ src/main/java/Entities/Booking.java
@@ -0,0 +1,67 @@
+package Entities;
+
+public class Booking {
+    private String lastname;
+    private String firstname;
+    private float totalprice;
+    private boolean depositpaid;
+    private BookingDates bookingdates;
+    private String additionalneeds;
+
+    public Booking(String lastname, String firstname, float totalprice, boolean depositpaid, BookingDates bookingdates, String additionalneeds) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.totalprice = totalprice;
+        this.depositpaid = depositpaid;
+        this.bookingdates = bookingdates;
+        this.additionalneeds = additionalneeds;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public float getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(float totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public boolean isDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(boolean depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public BookingDates getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(BookingDates bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
+}
